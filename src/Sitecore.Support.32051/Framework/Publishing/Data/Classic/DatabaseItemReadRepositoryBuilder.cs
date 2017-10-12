a2666d426735edb8ee4038011c5dc5495f9d3dbe
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-12 17:14:35 +0300

    Fix bug

--- src/Sitecore.Support.32051/Framework/Publishing/Data/Classic/DatabaseItemReadRepositoryBuilder.cs
+++ src/Sitecore.Support.32051/Framework/Publishing/Data/Classic/DatabaseItemReadRepositoryBuilder.cs
@@ -1,12 +1,11 @@
 ﻿using Sitecore.Framework.Publishing.Data.AdoNet;
-using Sitecore.Framework.Publishing.Data.Classic;
 using Sitecore.Framework.Publishing.Data.Repository;
 using Sitecore.Framework.Publishing.Item;
 using System;
 
 namespace Sitecore.Support.Framework.Publishing.Data.Classic
 {
-    public class DatabaseItemReadRepositoryBuilder : DefaultRepositoryBuilder<IIndexableItemReadRepository, ClassicItemRepository, IDatabaseConnection>
+    public class DatabaseItemReadRepositoryBuilder : DefaultRepositoryBuilder<IIndexableItemReadRepository, Sitecore.Support.Framework.Publishing.Data.Classic.ClassicItemRepository, IDatabaseConnection>
     {
         public DatabaseItemReadRepositoryBuilder(IServiceProvider services) : base(services)
         {

dd2c21124affc9c9d6d37aa53332f1f560d30674
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-12 17:06:18 +0300

    Initial commit

---
+++ src/Sitecore.Support.32051/Framework/Publishing/Data/Classic/DatabaseItemReadRepositoryBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Framework.Publishing.Data.AdoNet;
+using Sitecore.Framework.Publishing.Data.Classic;
+using Sitecore.Framework.Publishing.Data.Repository;
+using Sitecore.Framework.Publishing.Item;
+using System;
+
+namespace Sitecore.Support.Framework.Publishing.Data.Classic
+{
+    public class DatabaseItemReadRepositoryBuilder : DefaultRepositoryBuilder<IIndexableItemReadRepository, ClassicItemRepository, IDatabaseConnection>
+    {
+        public DatabaseItemReadRepositoryBuilder(IServiceProvider services) : base(services)
+        {
+
+        }
+    }
+}
